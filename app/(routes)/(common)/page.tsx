6f38d92eadc86bc0a5fdd7707d02bd485724fb74
nasangyeop <gigic5220@gmail.com>
2025-02-11 21:58:11 +0900

    피드 디자인 테스트

--- app/(routes)/(common)/page.tsx
+++ app/(routes)/(common)/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
 
   return (
       <div
-        className={'flex flex-col items-center scroll-x w-screen'}
+        className={'flex flex-col items-center scroll-x w-screen p-4'}
       >
           <NukkiFeed2Component/>
           <NukkiFeedComponent/>
--- app/_component/home/NukkiFeed2Component.tsx
+++ app/_component/home/NukkiFeed2Component.tsx
@@ -1,8 +1,25 @@
 "use client";
 
 import Image from "next/image";
+import {IoIosHeart} from "react-icons/io";
+
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {HiViewGrid} from "react-icons/hi";
 
 const NukkiFeed2Component = () => {
+
+
+    return <div
+        className={'grid grid-cols-2 w-full justify-center items-center h-[250px] gap-2'}
+    >
+        <TestItemComponent/>
+        <TestItemComponent/>
+    </div>
+}
+export default NukkiFeed2Component;
+
+const TestItemComponent = () => {
 
     const testImageList = [
         '/test_image1.png',
@@ -13,34 +30,84 @@ const NukkiFeed2Component = () => {
     ]
 
     return <div
-        className={'grid grid-cols-2 w-full justify-center items-center h-[250px] gap-2'}
+        className={'flex flex-col w-full h-[100%] rounded-lg border-primary-gray border-[1px] p-2'}
     >
         <div
-            className={'relative w-full h-[100%] rounded-lg border-primary-black border-[1px]'}
+            className={'flex items-center space-x-2 justify-between'}
         >
-            <Image
-                src={'/test_image1.png'}
-                alt={'test1'}
-                fill
-                style={{
-                    objectFit: 'contain'
-                }}
-                loading={'eager'}
-            />
+            <div
+                className={'flex items-center justify-center gap-2'}
+            >
+                <div
+                    className={'w-[25px] h-[25px] rounded-full bg-primary-nukki'}
+                >
+
+                </div>
+                <p
+                    className={'text-[12px]'}
+                >
+                    test_user
+                </p>
+            </div>
         </div>
         <div
-            className={'relative w-full h-[100%] rounded-lg border-primary-black border-[1px]'}
+            className={'w-full h-[100%]'}
         >
-            <Image
-                src={'/test_image2.png'}
-                alt={'test2'}
-                fill
-                style={{
-                    objectFit: 'contain'
-                }}
-                loading={'eager'}
-            />
+            <Swiper
+                slidesPerView={1}
+            >
+                {
+                    testImageList.map((slide) => (
+                        <SwiperSlide key={slide}>
+                            <div
+                                className={'w-full h-[150px] relative'}
+                            >
+                                <Image
+                                    src={slide}
+                                    alt={slide}
+                                    fill
+                                    style={{
+                                        objectFit: 'contain'
+                                    }}
+                                    loading={'eager'}
+                                />
+                                <div
+                                    className={'absolute bottom-2 right-2'}
+                                >
+                                    {/*<IoIosHeartEmpty
+                                            size={30}
+                                        />*/}
+                                    <IoIosHeart
+                                        size={30}
+                                        color={'red'}
+                                    />
+                                </div>
+                                <div
+                                    className={'absolute top-1 right-1'}
+                                >
+                                    <p
+                                        className={'text-primary-gray text-[12px]'}
+                                    >
+                                        1 / 10
+                                    </p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+        <div
+            className={'flex items-center justify-end'}
+        >
+            <div
+                className={'border-[0.5px] border-primary-black rounded-full p-[4px] bg-primary-black'}
+            >
+                <HiViewGrid
+                    size={20}
+                    color={'white'}
+                />
+            </div>
         </div>
     </div>
 }
-export default NukkiFeed2Component;
